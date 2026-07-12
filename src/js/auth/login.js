a68fdc50cf0fa9621a1bce5b67f9a7b8a2dab457
Omar-0419 <omarvizcainoorozco@gmail.com>
2026-07-12 17:12:13 -0500

    login page finished

---
+++ src/js/auth/login.js
@@ -0,0 +1,5 @@
+import "../../styles/global.css"
+import "../../styles/login.css"
+import "../../styles/input.css"
+
+console.log('Login cargado correctamente');
---
+++ src/js/auth/register.js
@@ -0,0 +1,5 @@
+import "../../styles/global.css"
+import "../../styles/register.css"
+import "../../styles/input.css"
+
+console.log("Register cargado correctamente");
