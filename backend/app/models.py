d57f07684df0f6cce23a6470efcd72ec7916fb0e
Cesar Vega <cesarvegamrales@gmail.com>
2026-07-14 15:52:00 -0500

    fix: arreglado la conexion con bd

---
+++ backend/app/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer, Date
+from datetime import date
+"""
+Define the tables as in db and the models
+"""
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "runners"
+
+    id_runner: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    lastname:Mapped[str] = mapped_column(String)
+    email : Mapped[str]= mapped_column(String)
+    phone_number: Mapped[str] = mapped_column(String)
+    password: Mapped[str] = mapped_column(String)
+    city: Mapped[str] = mapped_column(String)
+    date_register: Mapped[date] = mapped_column(Date)
+
