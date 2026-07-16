6cdc8dcf054a04aa1a5402a31025a43cfbad62ba
Andres barrios <anmabadi@outlook.com>
2026-07-16 12:21:09 -0500

    added middleware

--- app/main.py
+++ app/main.py
@@ -5,3 +5,13 @@ from routers.auth_router import router as auth_router
 app = FastAPI()
 
 app.include_router(auth_router)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
