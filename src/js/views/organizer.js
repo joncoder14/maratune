82edeeb128a1540c630bacc970288cfca1e27de2
jose visbal <josevisbal144@gmail.com>
2026-07-20 15:28:19 -0500

    functional search bar

--- src/js/views/organizer.js
+++ src/js/views/organizer.js
@@ -1,91 +1,107 @@
 import { getEvents, viewDetails } from "../services/eventService";
+import { newEvent } from "../services/eventService";
+import { searchEvents } from "../services/searchService";
 import "../../styles/global.css";
 import "../../styles/input.css";
 import "../../styles/dashboard-events.css";
 import { logout } from "../services/logoutService";
 import { router } from "../routes/router";
-import { newEvent } from "../services/eventService";
 import { dom } from "@fortawesome/fontawesome-svg-core";
 
 export function organizerView() {
-  return `<nav>
+  return `
+    <nav>
         <img src="src/assets/logo/logo-horizontal.png" alt="" width="150px">
 
         <ul>
             <li><a id="events-link" href="/organizer" data-link>Events</a></li>
             <li><a id="my-events-link" href="/my-events" data-link>My events</a></li>
         </ul>
-        
+
         <button id="logout-btn">Log out</button>
-    </nav>    
-    
+    </nav>
+
     <div class="dashboard">
 
         <main class="events-container">
 
             <header class="events-header">
-                <div class="">
-                    <h1>Search events</h1><br>
+                <div>
+                    <h1>Search events</h1>
+                    <br>
                     <p>Find your next race and push your limits.</p>
                 </div>
 
                 <button id="create-btn">Create event</button>
             </header>
 
-            
-
             <section class="search-section">
-                <input class="" type="text" placeholder="search a event">
-                <select name="search-status" id="search-status">
+
+                <input
+                    id="search-input"
+                    type="text"
+                    placeholder="Search an event">
+
+                <select id="search-status">
                     <option value="all">All</option>
                     <option value="open">Open</option>
                     <option value="close">Close</option>
                 </select>
-                <button class="search-btn">Search</button><br>
+
+                <button
+                    id="search-btn"
+                    class="search-btn">
+                    Search
+                </button>
+
             </section>
 
             <h2>Upcoming events</h2>
-            
+
             <section id="event-container" class="events-grid">
 
-            
             </section>
 
             <div id="form-container-father" class="hidden">
+
                 <div id="form-container">
-                    <form id="form-create" action="">
+
+                    <form id="form-create">
+
                         <div>
-                            <label for="name">Name:</label>
+                            <label>Name:</label>
                             <input name="nameEvent" type="text" required>
                         </div>
 
                         <div>
-                            <label for="description">Description:</label>
+                            <label>Description:</label>
                             <input name="description" type="text" required>
                         </div>
 
                         <div>
-                            <label for="date">Date event:</label>
+                            <label>Date event:</label>
                             <input name="date_event" type="date" required>
                         </div>
-                         <div>
-                            <label for="date_time">hour:</label>
+
+                        <div>
+                            <label>Hour:</label>
                             <input name="date_time" type="time" required>
                         </div>
 
                         <div>
-                            <label for="city">City:</label>
+                            <label>City:</label>
                             <input name="city" type="text" required>
                         </div>
 
                         <div>
-                            <label for="cups">Cups:</label>
+                            <label>Cups:</label>
                             <input name="cups" type="number" required>
                         </div>
 
                         <div>
-                            <label for="status">Status:</label>
-                            <select name="statusEvent" id="status" required>
+                            <label>Status:</label>
+
+                            <select name="statusEvent" required>
                                 <option value="open">Open</option>
                                 <option value="close">Close</option>
                             </select>
@@ -93,35 +109,38 @@ export function organizerView() {
 
                         <div id="form-btns">
                             <button id="done-btn" type="submit">Done</button>
-                            <button id="cancel-btn"type="button">Cancel</button>
+                            <button id="cancel-btn" type="button">Cancel</button>
                         </div>
+
                     </form>
+
                 </div>
+
             </div>
+
             <section id="viewDetails">
 
             </section>
-            
-            
-            
-            
+
         </main>
+
     </div>
     `;
 }
 
 export async function createEvent() {
-  const eventContainer = document.getElementById("events-container");
-  const donebtn = document.getElementById("done-btn");
+
   const user = JSON.parse(localStorage.getItem("user"));
+
   const createBtn = document.getElementById("create-btn");
-  const id_event_organizer = user.user.id;
   const formCreate = document.getElementById("form-create");
   const containerFather = document.getElementById("form-container-father");
-  const formContainer = document.getElementById("form-container");
-  console.log(user.user.id);
+  const cancelBtn = document.getElementById("cancel-btn");
+
+  const id_event_organizer = user.user.id;
 
   formCreate.addEventListener("submit", async (e) => {
+
     e.preventDefault();
 
     const {
@@ -144,66 +163,117 @@ export async function createEvent() {
       statusEvent,
       id_event_organizer,
     );
+
     if (created) {
+
       containerFather.classList.add("hidden");
-      alert("created!");
+
+      alert("Event created successfully!");
+
       history.pushState({}, "", "/organizer");
 
       router();
+
     }
+
   });
 
   createBtn.addEventListener("click", () => {
+
     containerFather.classList.remove("hidden");
+
   });
-  const cancelBtn = document.getElementById("cancel-btn");
+
   cancelBtn.addEventListener("click", () => {
+
     containerFather.classList.add("hidden");
+
   });
+
 }
 export async function organizerEvents() {
+
   const eventContainer = document.getElementById("event-container");
+
+  const input = document.getElementById("search-input");
+  const status = document.getElementById("search-status");
+  const searchBtn = document.getElementById("search-btn");
 
   const events = await getEvents();
 
-  console.log(events);
-
   await createEvent();
 
-  events.forEach((event) => {
-    eventContainer.innerHTML += `<article class="card-color">
+  function renderEvents(eventsList) {
 
-    
-                        <div class="event-card" >
-                        <span id="status-on-card" class="status ${event.status}">${event.status}</span>
-    
-    
-                        <div class="event-info">
-                            <h3>${event.name}</h3>
-                            <p>${event.city}</p>
-                            <p>${event.date_event}</p>
-                            <p>${event.cups}</p>
-                            
-                        </div>
-                        <div class="container-img">
-                            <img src="src/assets/logo/only-logo.png" width="200px" height="120px">
-                            
-    
-                        </div>
-                    </div>
-                    <div class="buttons">
-    
-                        <button id="suscribe-btn">Suscribe</button>
-                        <button data-id=${event.id_event} id="details-btn" class="details">View details</button>
-                    
-                    
-                    </div>
-    
-    
-                  
-                </article>`;
+    eventContainer.innerHTML = "";
+
+    eventsList.forEach((event) => {
+
+      eventContainer.innerHTML += `
+        <article class="card-color">
+
+          <div class="event-card">
+
+            <span id="status-on-card" class="status ${event.status}">
+              ${event.status}
+            </span>
+
+            <div class="event-info">
+              <h3>${event.name}</h3>
+              <p>${event.city}</p>
+              <p>${event.date_event}</p>
+              <p>${event.cups}</p>
+            </div>
+
+            <div class="container-img">
+              <img
+                src="src/assets/logo/only-logo.png"
+                width="200px"
+                height="120px">
+            </div>
+
+          </div>
+
+          <div class="buttons">
+
+            <button id="suscribe-btn">
+              Suscribe
+            </button>
+
+            <button
+              data-id="${event.id_event}"
+              class="details">
+              View details
+            </button>
+
+          </div>-
+
+        </article>
+      `;
+
+    });
+
+    viewDetails(eventsList);
+
+  }
+
+
+  renderEvents(events);
+
+  
+  searchBtn.addEventListener("click", () => {
+
+    const filteredEvents = searchEvents(
+      events,
+      input.value,
+      status.value
+    );
+
+    renderEvents(filteredEvents);
+
   });
-  viewDetails(events);
+
   logout();
+
 }
 
--- src/js/views/runner.js
+++ src/js/views/runner.js
@@ -1,14 +1,13 @@
-import { getEvents } from "../services/eventService"
-import "../../styles/global.css"
-import "../../styles/input.css"
-import "../../styles/dashboard-events.css"
-import { router } from "../routes/router"
+import { getEvents, viewDetails } from "../services/eventService";
+import { searchEvents } from "../services/searchService";
+import "../../styles/global.css";
+import "../../styles/input.css";
+import "../../styles/dashboard-events.css";
+import { router } from "../routes/router";
 import { logout } from "../services/logoutService";
-import { viewDetails } from "../services/eventService"
 
-export function runnerView(){
-    // const user = JSON.parse(localStorage.getItem("user"))
-    return `
+export function runnerView() {
+  return `
     <nav>
         <img id="logo" src="src/assets/logo/logo-horizontal.png" alt="" width="150px">
 
@@ -17,90 +16,141 @@ export function runnerView(){
         </ul>
 
         <button id="logout-btn">Log out</button>
-    </nav>    
-    
+    </nav>
+
     <div class="dashboard">
 
         <main class="events-container">
 
             <header class="events-header">
-                <div class="">
+                <div>
                     <h1>Search events</h1><br>
                     <p>Find your next race and push your limits.</p>
                 </div>
             </header>
 
-            
-
             <section class="search-section">
-                <input class="" type="text" placeholder="search a event">
-                <select name="search-status" id="search-status">
+                <input
+                    id="search-input"
+                    type="text"
+                    placeholder="Search an event">
+
+                <select id="search-status">
                     <option value="all">All</option>
                     <option value="open">Open</option>
                     <option value="close">Close</option>
                 </select>
-                <button class="search-btn">Search</button>
+
+                <button id="search-btn" class="search-btn">
+                    Search
+                </button>
             </section>
 
             <h2>Upcoming events</h2>
-            
+
             <section id="event-container" class="events-grid">
 
-            
             </section>
 
             <section id="viewDetails">
 
             </section>
-            
-            
-            
-            
+
         </main>
+
     </div>
-    `
+    `;
 }
 
-export async function runnerEvents(){
-    console.log("runner cargado");
-    const eventContainer = document.getElementById("event-container")
-    
-        const events = await getEvents() 
-    
-        events.forEach(event => {
-            eventContainer.innerHTML += `<article class="card-color">
-    
-                    <div class="event-card" >
-                    <span id="status-on-card" class="status ${event.status}">${event.status}</span>
-    
-    
+export async function runnerEvents() {
+
+    const eventContainer = document.getElementById("event-container");
+    const input = document.getElementById("search-input");
+    const status = document.getElementById("search-status");
+    const searchBtn = document.getElementById("search-btn");
+
+    const events = await getEvents();
+
+    function renderEvents(eventsList) {
+
+        eventContainer.innerHTML = "";
+
+        eventsList.forEach((event) => {
+
+            eventContainer.innerHTML += ` <article class="card-color">
+
+                    <div class="event-card">
+
+                        <span id="status-on-card" class="status ${event.status}">
+                            ${event.status}
+                        </span>
+
                         <div class="event-info">
+
                             <h3>${event.name}</h3>
-                            <p><i id="icon-location" class="fa-solid fa-location-dot"></i> ${event.city}</p>
-                            <p><i id="icon-schedule" class="fa-regular fa-calendar"></i> ${event.date_event}</p>
-                            <p><i id="icon-dollar" class="fa-solid fa-dollar-sign"></i> ${event.cups}</p>
-                            
+
+                            <p>
+                                <i id="icon-location" class="fa-solid fa-location-dot"></i>
+                                ${event.city}
+                            </p>
+
+                            <p>
+                                <i id="icon-schedule" class="fa-regular fa-calendar"></i>
+                                ${event.date_event}
+                            </p>
+
+                            <p>
+                                <i id="icon-dollar" class="fa-solid fa-dollar-sign"></i>
+                                ${event.cups}
+                            </p>
+
                         </div>
+
                         <div class="container-img">
-                            <img src="src/assets/logo/only-logo.png" width="200px" height="120px">
-                            
-    
+                            <img
+                                src="src/assets/logo/only-logo.png"
+                                width="200px"
+                                height="120px">
                         </div>
+
                     </div>
+
                     <div class="buttons">
-    
-                        <button id="suscribe-btn">Suscribe</button>
-                        <button data-id=${event.id_event} id="details-btn" class="details">View details</button>
-                    
-                    
+
+                        <button id="suscribe-btn">
+                            Suscribe
+                        </button>
+
+                        <button
+                            data-id="${event.id_event}"
+                            class="details">
+                            View details
+                        </button>
+
                     </div>
-    
-    
-                  
-                </article>`
+
+                </article>
+            `;
         });
-        viewDetails(events);
-        logout();
+
+        viewDetails(eventsList);
+    }
+
+    renderEvents(events);
+
+    searchBtn.addEventListener("click", () => {
+
+        const filteredEvents = searchEvents(
+            events,
+            input.value,
+            status.value
+        );
+
+        renderEvents(filteredEvents);
+
+    });
+
+    logout();
 }
         
 
